d8140dfbc2ae93c8cffe10ad6e99bbdd016daef9
Ia Vaquilar <iavaquilar@gmail.com>
2021-08-23 15:51:30 +0100

    Uploaded package using twine

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='ia_first_package',
       version="1.0",
-      description="Project Description",
+      description="A test to package sharing",
       packages=find_packages(),
       install_requires=requirements,
+      url='https://github.com/iavaq/ia_first_package',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
